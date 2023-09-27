3d3f6fbc7bc3d9b9e694e390ef66da8ca1b0217f
midhunrajk <midhunrajk009@gmail.com>
2023-09-27 13:47:34 +0530

    idk2

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -7,7 +7,7 @@ const { test, registerUser, loginUser, getProfile } = require('../controllers/au
 router.use(
     cors({
         credentials: true,
-        origin: "http://localhost:3000"
+        origin: "http://login-register-cyan.vercel.app"
     })
 );
 
