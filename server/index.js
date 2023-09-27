f778aa7f40a6e6fe649a8a154ccdc19a28740b64
midhunrajk <midhunrajk009@gmail.com>
2023-09-27 14:55:57 +0530

    idk actually2

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const cookieParser = require('cookie-parser');
 const app = express();
 
 app.use(cors({
-    origin: [""],
+    origin: ["https://login-register-frontend-phi.vercel.app/"],
     methods: ['POST', 'GET'],
     credentials: true
 }))
